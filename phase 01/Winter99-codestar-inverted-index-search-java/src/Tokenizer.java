0cadc8a63712ac85d738b1bac84cdd24e4c9535e
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-02-23 17:57:25 +0330

    phase 1 : docs for Tokenizer class added

--- phase 01/Winter99-codestar-inverted-index-search-java/src/Tokenizer.java
+++ phase 01/Winter99-codestar-inverted-index-search-java/src/Tokenizer.java
@@ -1,15 +1,33 @@
 public class Tokenizer {
 
+    /**
+     * Extract content of the file to tokens
+     *
+     * @param content String of text in the file to be tokenized
+     * @return list of all valid tokens (Just alpha numeric chars are valid in tokens)
+     */
     public String[] getTokens(String content) {
         content = normalizeContent(content);
         return content.split("\\W+");
     }
 
+    /**
+     * Converts token to its normal form (lower case chars)
+     *
+     * @param word input string
+     * @return normalized valid token
+     */
     public String normalizeToken(String word) {
         word = word.toLowerCase();
         return word;
     }
 
+    /**
+     * Converts token to its normal form (lower case chars)
+     *
+     * @param content input string
+     * @return normalized content
+     */
     private String normalizeContent(String content) {
         content = content.toLowerCase();
         return content;
