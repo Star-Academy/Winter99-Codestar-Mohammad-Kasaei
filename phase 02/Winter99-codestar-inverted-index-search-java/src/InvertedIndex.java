d73f854f84f8b32c41ec95adb1ce0da78a5804a1
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-02-24 23:44:04 +0330

    phase 2 : JavaDoc added for InvertedIndex.java

--- phase 02/Winter99-codestar-inverted-index-search-java/src/InvertedIndex.java
+++ phase 02/Winter99-codestar-inverted-index-search-java/src/InvertedIndex.java
@@ -4,12 +4,23 @@ import java.util.Map;
 import java.util.Set;
 
 public class InvertedIndex {
+    /**
+     * The whole inverted index structure to save all the docs related to one
+     * token and search quickly
+     */
     private final Map<Token, Set<Document>> data;
 
     public InvertedIndex() {
         data = new HashMap<>();
     }
 
+
+    /**
+     * Add one document to the inverted index
+     *
+     * @param doc     documents to add to inverted index
+     * @param content content of the documented to be added
+     */
     public void addDocument(Document doc, String content) {
         Token[] tokens = Token.textToTokens(content);
         for (Token t : tokens) {
@@ -20,12 +31,23 @@ public class InvertedIndex {
         }
     }
 
+    /**
+     * Add multiple docs to the inverted index
+     *
+     * @param docs documents to add to inverted index
+     */
     public void addDocuments(Map<Document, String> docs) {
         for (Map.Entry<Document, String> entry : docs.entrySet()) {
             addDocument(entry.getKey(), entry.getValue());
         }
     }
 
+    /**
+     * Query one token in whole documents
+     *
+     * @param token a token to search the docs
+     * @return returns the set of all documents containing the token
+     */
     public Set<Document> query(Token token) {
         return data.getOrDefault(token, new HashSet<>());
     }
