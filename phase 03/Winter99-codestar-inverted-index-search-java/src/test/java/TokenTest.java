3c0b8336b4c8cf6d5a46e7cda47f7d8692786dd1
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-03-01 18:13:08 +0330

    phase 3 : TokenTest added.

---
+++ phase 03/Winter99-codestar-inverted-index-search-java/src/test/java/TokenTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class TokenTest {
+
+    @Test
+    public void testEquals() {
+        final String tokenString = "My_TOKEN:)";
+        final Token token = new Token(tokenString);
+        assertNotEquals(null, token);
+        assertNotEquals("MyToken", token);
+        assertEquals(token, new Token(tokenString));
+        assertEquals(token, new Token("my_token"));
+    }
+
+    @Test
+    public void testHashCode() {
+        final Token token = new Token("(((----??tokenMe:)");
+        final Token token2 = new Token(token.toString());
+        assertEquals(token2.hashCode(), token.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        final Token token = new Token("(((----??tokenMe:)");
+        assertEquals("tokenme", token.toString());
+    }
+}
