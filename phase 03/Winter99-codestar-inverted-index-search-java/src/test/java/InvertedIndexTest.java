41975946f9254011a465faa499655ef01b8d177f
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-03-02 17:16:38 +0330

    phase 3 : testing dependencies corrected.

--- phase 03/Winter99-codestar-inverted-index-search-java/src/test/java/InvertedIndexTest.java
+++ phase 03/Winter99-codestar-inverted-index-search-java/src/test/java/InvertedIndexTest.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 
 public class InvertedIndexTest {
