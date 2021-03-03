efd70b7c570ea37519feeef494c9946d41312892
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-03-03 17:36:23 +0330

    phase 3 : main function refactored.

--- phase 03/Winter99-codestar-inverted-index-search-java/src/main/java/Main.java
+++ phase 03/Winter99-codestar-inverted-index-search-java/src/main/java/Main.java
@@ -3,17 +3,21 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        final String path = "EnglishData";
-        final InvertedIndex invertedIndex = new InvertedIndex();
-        final TextFileReader fileReader = new TextFileReader(path);
-        final Map<Document, String> docs = fileReader.readAllFileInFolder();
-        invertedIndex.addDocuments(docs);
-        final QueryEngine queryEngine = new QueryEngine(invertedIndex);
+        final QueryEngine queryEngine = new QueryEngine(initializeInvertedIndex());
         final CommandLine cmd = new CommandLine(
                 queryEngine::advancedSearch,
                 CommandLine.DEFAULT_TERMINATOR,
                 System.in,
                 System.out);
         cmd.start();
+    }
+
+    private static InvertedIndex initializeInvertedIndex() {
+        final String path = "EnglishData";
+        final InvertedIndex invertedIndex = new InvertedIndex();
+        final TextFileReader fileReader = new TextFileReader(path);
+        final Map<Document, String> docs = fileReader.readAllFileInFolder();
+        invertedIndex.addDocuments(docs);
+        return invertedIndex;
     }
 }
