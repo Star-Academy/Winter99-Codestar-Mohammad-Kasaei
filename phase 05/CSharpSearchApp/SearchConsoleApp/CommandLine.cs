13e7ac2d278a5f4d0b0395c2c41844fd6fe544f5
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-03-11 15:43:04 +0330

    phase 5 : SearchConsoleApp implemented.

---
+++ phase 05/CSharpSearchApp/SearchConsoleApp/CommandLine.cs
@@ -0,0 +1,41 @@
+﻿using SearchLibrary;
+using System;
+using System.Linq;
+
+namespace SearchConsoleApp
+{
+    public class CommandLine
+    {
+
+        private readonly Func<string[], DocumentSet> handle;
+
+        public interface ICommandLine
+        {
+            public DocumentSet SearchWordsList(string[] words);
+        }
+
+        public CommandLine(Func<string[] , DocumentSet> handle)
+        {
+            this.handle = handle;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to Search engine\n\nEnter List of Queries:");
+            var line = Console.ReadLine();
+            var docs = handle(line.Split(" "));
+            if(docs.GetEnumerable().Any())
+            {
+                foreach (var doc in docs.GetEnumerable())
+                {
+                    Console.WriteLine(@$"Document {doc}");
+                }
+            }
+            else
+            {
+                Console.WriteLine(@$"No document found");
+            }
+            
+        }
+    }
+}
---
+++ phase 05/CSharpSearchApp/SearchConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using SearchLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace SearchConsoleApp
+{
+    class Program
+    {
+        private class CommandLineQueryEngine : CommandLine.ICommandLine
+        {
+            public DocumentSet SearchWordsList(string[] words)
+            {
+                return queryEngine.AdvancedSearch(words);
+            }
+        }
+        private static readonly string dataPath = @"..\..\..\..\EnglishData";
+        private static readonly Dictionary<Document, string> files = TextFileReader.ReadAllFilesInDirectory(dataPath);
+        private static readonly InvertedIndex invertedIndex = new(files);
+        private static readonly QueryEngine queryEngine = new(invertedIndex);
+//        private static readonly /**/
+
+        static void Main(string[] args)
+        {
+            CommandLine commandLine = new((words) => queryEngine.AdvancedSearch(words));
+            commandLine.Start();
+        }
+    }
+}
