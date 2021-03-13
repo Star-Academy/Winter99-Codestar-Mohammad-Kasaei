247e97c501b3c8516b954a1dae15416d2453718a
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-03-13 20:56:53 +0330

    phase 5 : refactor in InvertedIndexTest.

--- phase 05/CSharpSearchApp/SearchLibrary.Test/InvertedIndexTest.cs
+++ phase 05/CSharpSearchApp/SearchLibrary.Test/InvertedIndexTest.cs
@@ -8,6 +8,13 @@ namespace SearchLibrary.Test
     [ExcludeFromCodeCoverage]
     public class InvertedIndexTest
     {
+        private readonly Dictionary<Document, string> sampleData = new()
+            {
+                {new Document("doc1") , "this is content of the file one" },
+                {new Document("doc2") , "this is content of the file two" }
+            };
+
+
         /// <summary>
         /// just to make sure that the proper constructor is implemented.
         /// </summary>
@@ -50,12 +57,7 @@ namespace SearchLibrary.Test
         [Fact]
         public void QuerySimpleWithConsturctorDataTest()
         {
-            var baseData = new Dictionary<Document, string>
-            {
-                {new Document("doc1") , "this is content of the file one" },
-                {new Document("doc2") , "this is content of the file two" }
-            };
-            var invertedIndex = new InvertedIndex(baseData);
+            var invertedIndex = new InvertedIndex(sampleData);
 
             var expected = new DocumentSet(new HashSet<Document>
             {
@@ -88,12 +90,7 @@ namespace SearchLibrary.Test
         public void QuerySimpleWithAddDocumentsTest()
         {
             var invertedIndex = new InvertedIndex();
-            var baseData = new Dictionary<Document, string>
-            {
-                {new Document("doc1") , "this is content of the file one" },
-                {new Document("doc2") , "this is content of the file two" }
-            };
-            invertedIndex.AddDocuments(baseData);
+            invertedIndex.AddDocuments(sampleData);
             var expected = new DocumentSet(new HashSet<Document>
             {
                 new Document("doc1"),
@@ -108,12 +105,7 @@ namespace SearchLibrary.Test
         public void QueryEmptyResultTest()
         {
             var invertedIndex = new InvertedIndex();
-            var baseData = new Dictionary<Document, string>
-            {
-                {new Document("doc1") , "this is content of the file one" },
-                {new Document("doc2") , "this is content of the file two" }
-            };
-            invertedIndex.AddDocuments(baseData);
+            invertedIndex.AddDocuments(sampleData);
             var expected = new DocumentSet(new HashSet<Document>());
             var actual = invertedIndex.SearchTokenInDocuments(new Token("Mohammad"));
             Assert.Equal(expected, actual);
