a7726dce8c4156071667f9849b7cb63d5d15dd8b
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2021-03-13 16:56:37 +0330

    phase 5 : refactor in TextFileReaderTest class

--- phase 05/CSharpSearchApp/SearchLibrary.Test/TextFileReaderTest.cs
+++ phase 05/CSharpSearchApp/SearchLibrary.Test/TextFileReaderTest.cs
@@ -8,32 +8,41 @@ namespace SearchLibrary.Test
     [ExcludeFromCodeCoverage]
     public class TextFileReaderTest
     {
+
+        private readonly string fileContent1 = "This is the content of first file";
+        private readonly string fileContent2 = "This is the content of second file";
+        private readonly string directoryName = "testFolderDirectory";
+        private readonly string doc1Name = "doc1";
+        private readonly string doc2Name = "doc2";
+
+
+        private void Setup()
+        {
+            Directory.CreateDirectory(directoryName);
+            File.WriteAllText(@$"{directoryName}\{doc1Name}", fileContent1);
+            File.WriteAllText(@$"{directoryName}\{doc2Name}", fileContent2);
+        }
+
+        private void TearDown()
+        {
+            File.Delete(@$"{directoryName}\{doc1Name}");
+            File.Delete(@$"{directoryName}\{doc2Name}");
+            Directory.Delete(directoryName);
+        }
+
+
         [Fact]
         public void ReadAllFilesInFolderTest()
         {
-            // setup 
-            var fileContent1 = "This is the content of first file";
-            var fileContent2 = "This is the content of second file";
-            var directoryName = "testFolderDirectory";
-            var doc1Name = "doc1";
-            var doc2Name = "doc2";
+            Setup();
             var expected = new Dictionary<Document, string>()
             {
                 { new Document(doc1Name) , fileContent1},
                 { new Document(doc2Name) , fileContent2},
             };
-            Directory.CreateDirectory(directoryName);
-            File.WriteAllText(@$"{directoryName}\{doc1Name}", fileContent1);
-            File.WriteAllText(@$"{directoryName}\{doc2Name}", fileContent2);
-
-            // test
             var actual = TextFileReader.ReadAllFilesInDirectory(directoryName);
             Assert.Equal(expected, actual);
-
-            // tear down
-            File.Delete(@$"{directoryName}\{doc1Name}");
-            File.Delete(@$"{directoryName}\{doc2Name}");
-            Directory.Delete(directoryName);
+            TearDown();
         }
     }
 }
